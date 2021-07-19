c06e0a0d8973f25a1de72f5afff360f8e46eb1a3
moon <gaon4336@naver.com>
2021-07-19 22:50:28 +0900

    solved encoding problem

--- basic_Workspace/crawling/stock_crawling/stock1.py
+++ basic_Workspace/crawling/stock_crawling/stock1.py
@@ -1,14 +1,18 @@
+#-*- encoding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 
 
 
 url = "https://finance.naver.com/sise/sise_market_sum.nhn"
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 
-res = requests.get(url, headers={'User-Agent':'Mozila/5.0'})
-print(res.headers)                              #res의 charset='EUC-KR'
-text = res.content.decode('utf-8','replace')    #res의 content를 'UTF-8로 변경'   
+res = requests.get(url, headers=headers)            
+
+#print(res.headers)                              #res의 charset='EUC-KR'        #python의 형태는 
+text = res.content.decode('euc-kr','replace')    #res의 content를 'EUC-KR로 변경'   
 
 soup = BeautifulSoup(text,'html.parser')
 wholeFile = open('dataA.html','w',encoding='utf-8')
 wholeFile.write(text)       #BeautifulSoup 형식 저장(x) -> text로 저장
+
