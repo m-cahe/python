4dc8d2b9fd74d0a53633bf53816c23e9ba2411c8
moon <gaon4336@naver.com>
2021-07-27 01:27:34 +0900

    Excluding duplicate language

--- basic_Workspace/translator/translator.py
+++ basic_Workspace/translator/translator.py
@@ -6,12 +6,14 @@ translator = Translator()
 sentence=input("무슨 말을 번역할래? ")
 print(translator.detect(sentence).lang, ' : ' , sentence)   #detect:감지하다v
 
-result_ko = translator.translate(sentence, dest="ko")
-result_en = translator.translate(sentence, dest='en')       #translate(${sentence}, dest='${language}) / dest(=destination)
-result_ja = translator.translate(sentence, dest='ja')
-result_vi = translator.translate(sentence, dest='vi')
+lan=["ko","en","ja","vi"]
+result_ko = translator.translate(sentence, dest=lan[0])
+result_en = translator.translate(sentence, dest=lan[1])       #translate(${sentence}, dest='${language}) / dest(=destination)
+result_ja = translator.translate(sentence, dest=lan[2])
+result_vi = translator.translate(sentence, dest=lan[3])
 
-print(result_ko.dest," : ",result_ko.text)
-print(result_en.dest,' : ',result_en.text)
-print(result_ja.dest,' : ',result_ja.text)
-print(result_vi.dest,' : ',result_vi.text)
+result_lan = [result_ko, result_en, result_ja, result_vi]
+for a in result_lan:
+    if(translator.detect(sentence).lang == a.dest):            #중복언어 제외
+        continue
+    print(a.dest," : ",a.text)
