e9bc9251ee821d146dc35e25538cf82d0a184834
moon <gaon4336@naver.com>
2021-07-27 01:03:56 +0900

    create a translator

--- basic_Workspace/translator/translator.py
+++ basic_Workspace/translator/translator.py
@@ -1,5 +1,17 @@
-from googletrans import Translator
-
+from json import detect_encoding
+from googletrans import Translator                      #googletrans -> translator -> detect/translate
+                                                        #4.0.0 이상
 translator = Translator()
-sentence="안녕"
-print(translator.detect(text='안녕', desc='en',src='ko'))
+
+sentence=input("무슨 말을 번역할래? ")
+print(translator.detect(sentence).lang, ' : ' , sentence)   #detect:감지하다v
+
+result_ko = translator.translate(sentence, dest="ko")
+result_en = translator.translate(sentence, dest='en')       #translate(${sentence}, dest='${language}) / dest(=destination)
+result_ja = translator.translate(sentence, dest='ja')
+result_vi = translator.translate(sentence, dest='vi')
+
+print(result_ko.dest," : ",result_ko.text)
+print(result_en.dest,' : ',result_en.text)
+print(result_ja.dest,' : ',result_ja.text)
+print(result_vi.dest,' : ',result_vi.text)
